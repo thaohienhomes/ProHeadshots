89d5189c84bb040330b0ab7302267a12e506363a
Johnny Tran <69196569+johnnetr2@users.noreply.github.com>
2025-02-03 21:21:49 +0100

    Update getAstriaPrompts.ts

--- src/action/getAstriaPrompts.ts
+++ src/action/getAstriaPrompts.ts
@@ -15,32 +15,39 @@ const tuneIdSchema = z.string().min(1);
 
 export async function getAstriaPrompts(tuneId: string) {
   try {
-    // Validate tune ID
     const validTuneId = tuneIdSchema.parse(tuneId);
     const API_KEY = process.env.ASTRIA_API_KEY;
+    let allPrompts: Prompt[] = [];
+    let offset = 0;
+    const PAGE_SIZE = 20;
 
-    const response = await fetch(
-      `https://api.astria.ai/tunes/${validTuneId}/prompts`,
-      {
-        headers: {
-          Authorization: `Bearer ${API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        cache: "no-store",
+    while (true) {
+      const response = await fetch(
+        `https://api.astria.ai/tunes/${validTuneId}/prompts?offset=${offset}`,
+        {
+          headers: {
+            Authorization: `Bearer ${API_KEY}`,
+            "Content-Type": "application/json",
+          },
+          cache: "no-store",
+        }
+      );
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Failed to fetch prompts: ${response.status} ${errorText}`);
       }
-    );
 
-    if (!response.ok) {
-      const errorText = await response.text();
-      console.error("Response status:", response.status);
-      console.error("Response headers:", Object.fromEntries(response.headers));
-      console.error("Error details:", errorText);
-      throw new Error(`Failed to fetch prompts: ${response.status} ${errorText}`);
+      const data = await response.json();
+      if (data.length === 0) break; // No more prompts to fetch
+
+      allPrompts = [...allPrompts, ...data];
+      offset += PAGE_SIZE;
     }
 
-    const data = await response.json();
-    //console.log(data);
-    return data as Prompt[];
+    const uniqueUrls = new Set(allPrompts.flatMap((prompt: Prompt) => prompt.images)).size;
+    console.log("Total Unique URLs from Astria:", uniqueUrls);
+    return allPrompts;
   } catch (error) {
     console.error("Error fetching Astria prompts:", error);
     throw error;
